1912ce9a4ed8b8aa93b8f4364350a7c00b3526a7
ShouryaSaran <saran.shourya0211@gmail.com>
2026-02-05 22:33:14 +0530

    first video

---
+++ video1/variable.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accoundEmail = "shourya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2
+
+accountEmail = "for@gmail.com"
+
+console.log(accountId);
+
+console.table([accoundEmail,accountId,accountPassword,accountCity,accountState]);
